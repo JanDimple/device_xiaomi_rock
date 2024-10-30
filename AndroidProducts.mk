6e9693fe7514a9d94b6b955e587e7bc590537c3c
Shakib <its.shakib@proton.me>
2024-10-31 04:46:14 +0700

    Rock: Bringup RisingOS-14
    Signed-off-by: Shakib <its.shakib@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,9 +16,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_rock.mk
+    $(LOCAL_DIR)/lineage_rock.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_rock-ap2a-user \
-    aosp_rock-ap2a-userdebug \
-    aosp_rock-ap2a-eng
+    lineage_rock-ap2a-user \
